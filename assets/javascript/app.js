373edfc6eba72e878584f2f75fa0fec57d9dd4cf
Taylor Zettel <tzettel@gmail.com>
2019-11-08 18:20:39 -0500

    created (placeholder answer object, placeholder question population function, correct answer function)

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,14 +5,53 @@ $(document).ready(function() {
   var result = false;
   var score = false;
 
+  $('#start-button').click(function() {
+    $('#start-button').css('display', 'none');
+    $('.st-question').css('display', 'block');
+    questionPop();
+  })
+
+  var questions = {
+    question1: {
+      q: 'Question about red?',
+      a1: 'blue',
+      a2: 'blue',
+      a3: 'red',
+      a4: 'blue',
+      ca: 'red',
+      yes: "yup, it's red!",
+      no: "No! It's red! You suck!"
+    }
+
+  }
+
   function reset() {
     $('.rst').css('display', 'none');
   }
 
-  $('#start-button').click(function() {
-    $('#start-button').css('display', 'none');
-    $('.st-question').css('display', 'block');
-  })
+  function questionPop() {
+    $('#question').text(questions.question1.q);
+    $('#answer1').text(questions.question1.a1);
+    $('#answer2').text(questions.question1.a2);
+    $('#answer3').text(questions.question1.a3);
+    $('#answer4').text(questions.question1.a4);
+  }
+
+  $('.btn-block').click(function() {
+    var btnText = $(this).text();
+    console.log(btnText);
+    console.log(questions.question1.ca);
+    if (btnText == questions.question1.ca) {
+      $('#result').text('Correct! The answer is ' + questions.question1.ca + '!');
+      $('.st-question').css('display', 'none');
+      $('#result').css('display', 'block');
+      }
+    else {
+      $('#result').text('Wrong! The answer is ' + questions.question1.ca + '!');
+      $('.st-question').css('display', 'none');
+      $('#result').css('display', 'block');
+    }
+    })
 
   reset();
 });  
